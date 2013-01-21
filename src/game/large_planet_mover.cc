77ccb519ced1b5d41f26a828ef94a6eaa6631cb3
Christopher Hebert <hebert.christopherj@gmail.com>
2013-01-20 20:36:06 -0800

    adding large planet movement

--- src/game/large_planet_mover.cc
+++ src/game/large_planet_mover.cc
@@ -1,19 +1,71 @@
 #include "large_planet_mover.h"
+#include "util/glm_util.h"
 
-void LargePlanetMover::TurnLeft() { xz_rotation_.angle += 20; }
-void LargePlanetMover::TurnRight() { xz_rotation_.angle -= 20; }
+static float kJumpSlowdownHeld = 0.01f;
+static float kJumpSlowdown = 0.02f;
+static float kJumpSpeed = 0.3f;
+
+void LargePlanetMover::TurnLeft() {
+   rotate_type_ = ROTATE_LEFT;
+}
+void LargePlanetMover::TurnRight() {
+   rotate_type_ = ROTATE_RIGHT;
+}
+
+void LargePlanetMover::StopMoving() {
+   rotate_type_ = ROTATE_NONE;
+}
 
 LargePlanetMover::LargePlanetMover(Planet* planet) :
       planet_(planet),
-      planet_rotater_(NULL)
+      theta_(90.0f),
+      phi_(90.0f),
+      rotate_type_(ROTATE_NONE)
 {
    xy_rotation_.axis = glm::vec3(0.0f, 0.0f, 1.0f);
    xz_rotation_.axis = glm::vec3(0.0f, 1.0f, 0.0f);
 }
 
+inline float radians(float degrees) {
+   return degrees * (atan(1)*4.0) / 180.0f;
+}
+
+const glm::vec3 LargePlanetMover::position() const {
+   return planet_->center() +
+      glm::vec3(
+         radius_ * std::sin(radians(theta_)) *
+                   std::cos(radians(phi_)),
+         radius_ * std::sin(radians(theta_)) *
+                   std::sin(radians(phi_)),
+         radius_ * std::cos(radians(theta_)));
+}
+
 void LargePlanetMover::Update() {
    RotateBottomTowardPlanet();
-   planet_rotater_->Update(position_, xy_rotation_, &is_jumping_);
+
+   if (rotate_type_ == ROTATE_LEFT) {
+      xz_rotation_.angle += 10.0f;
+   } else if (rotate_type_ == ROTATE_RIGHT) {
+      xz_rotation_.angle -= 10.0f;
+   }
+
+   if (is_jumping_ || is_falling_) {
+      if (jump_held_ && jump_speed_ > 0.0f)
+         jump_speed_ -= kJumpSlowdownHeld;
+      else
+         jump_speed_ -= kJumpSlowdown;
+      if (jump_speed_ <= 0.0f) {
+         is_jumping_ = false;
+         is_falling_ = true;
+      }
+      radius_ += jump_speed_;
+      if (radius_ <= planet_->radius()) {
+         jump_speed_ = kJumpSpeed;
+         radius_ = planet_->radius();
+         is_falling_ = false;
+      }
+   }
+
    UpdateMeshTransform();
 }
 
@@ -22,13 +74,19 @@ float angle_of(const glm::vec3& vec) {
    return 180.0f * std::atan2(vec.y, vec.x) / (atan(1) * 4);
 }
 
+void LargePlanetMover::FallToPlanet() {
+   is_jumping_ = false;
+   is_falling_ = true;
+}
+
 void LargePlanetMover::RotateBottomTowardPlanet() {
-   xy_rotation_.angle = angle_of(position_ - planet_->center()) - 90.0f;
+   xy_rotation_.angle = angle_of(position() - planet_->center()) - 90.0f;
 }
 
 void LargePlanetMover::UpdateMeshTransform() const {
    glm::mat4 transform;
-   transform *= glm::translate(position_);
+   glm_util::Print(position());
+   transform *= glm::translate(position());
    transform *= glm::rotate(xy_rotation_.angle, xy_rotation_.axis);
    transform *= glm::rotate(xz_rotation_.angle, xz_rotation_.axis);
    SceneNode::Get("player")->set_transformation(transform);
@@ -39,7 +97,5 @@ void LargePlanetMover::MoveForward() {
 
 void LargePlanetMover::set_planet(Planet* planet) {
    planet_ = planet;
-   if (planet_rotater_)
-      delete planet_rotater_;
-   planet_rotater_ = new PlanetRotater(planet->center(), planet->radius(), position_);
+   FallToPlanet();
 }
--- src/game/large_planet_mover.h
+++ src/game/large_planet_mover.h
@@ -17,24 +17,33 @@ class LargePlanetMover {
    void MoveForward();
    void TurnLeft();
    void TurnRight();
-   void StopMoving() {}
+   void StopMoving();
+   void FallToPlanet();
 
    bool is_attached_to(Planet* planet) const { return planet_ == planet; }
    bool is_jumping() const { return is_jumping_; }
 
-   const glm::vec3& position() const { return position_; }
+   const glm::vec3 position() const;
 
   private:
+   enum RotateType { ROTATE_LEFT, ROTATE_RIGHT, ROTATE_NONE };
    void UpdateMeshTransform() const;
    void RotateBottomTowardPlanet();
 
+   RotateType rotate_type_;
+
    Planet* planet_;
-   glm::vec3 position_;
+   float radius_;
+   float theta_;
+   float phi_;
+
    Rotation xy_rotation_;
    Rotation xz_rotation_;
-   PlanetRotater* planet_rotater_;
 
+   float jump_speed_;
    bool is_jumping_;
+   bool is_falling_;
+   bool jump_held_;
 };
 
 #endif
