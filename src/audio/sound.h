15f5f49451ab26d0a7f2fcfd1c247b14e8d81f2e
elindahl <e17lindahl@gmail.com>
2013-01-31 00:32:13 -0800

    Adding new sound stuff

---
+++ src/audio/sound.h
@@ -0,0 +1,16 @@
+#include <string.h>
+
+const std::string TECHNO_SAMPLE = "src/audio/samples/tek2beat178.wav";
+const std::string SIMPLE_SAMPLE = "src/audio/samples/sound.wav";
+const std::string NODENS = "src/audio/samples/nodens.mp3";
+const std::string SOLACE = "src/audio/samples/solace.mp3";
+
+void initSound();
+
+void playSampleWAV(std::string path);
+
+void loopSampleWAV(std::string path);
+
+void playMusic(std::string path);
+
+void loopMusic(std::string path);
