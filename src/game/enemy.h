6b5f1932fae97cf0459bf95f51eacf5bb2aa7135
erik <erik@pcwinner.(none)>
2013-02-28 00:41:50 -0800

    enemy core files to be extended for the hand and other enemies

---
+++ src/game/enemy.h
@@ -0,0 +1,27 @@
+#ifndef ENEMY_H_
+#define ENEMY_H_
+
+#include <assert.h>
+#include <string.h>
+#include <glm/glm.hpp>
+#include "../spatial_heirarchy/collidable_entity.h"
+
+class Planet;
+class SceneNode;
+
+class Enemy : public CollidableEntity {
+   public:
+    Enemy() {}
+    Enemy(Planet *planet, float theta, const std::string& id);
+    virtual ~Enemy();
+    virtual bool Update();
+    glm::vec3 position();
+
+   private:
+    void UpdateMeshPosition();
+
+    Planet* planet_;
+    float theta_;
+    float radius_;
+    SceneNode *mesh_;
+};
